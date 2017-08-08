5c042cd5c26e1df82d8c263b8cd26d57c092150b
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-08-08 16:37:55 +0200

    add minified library

--- script.release.sh
+++ script.release.sh
@@ -3,5 +3,5 @@
 ##
 
 echo "Create uglified library..."
-node ./node_modules/uglify-js/bin/uglifyjs ./src/bitcoinjs.modified.js > ./src/dist/bitcoinjs.min.js
-echo "Library uglified into to ./src/dist/bitcoinjs.min.js"
+node ./node_modules/uglify-js/bin/uglifyjs ./src/bitcoinjs.modified.js > ./dist/bitcoinjs.min.js
+echo "Library uglified into to ./dist/bitcoinjs.min.js"
