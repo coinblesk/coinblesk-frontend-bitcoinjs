f969eb6f2f7cf8a13af2a2cf3209c1b82a5dad41
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-08-10 20:12:00 +0200

    remove /dist variant of Bitcoin.JS: modified version is now relevant

--- script.release.sh
+++
@@ -1,7 +0,0 @@
-##
-## Creates a uglified version of the source file in /src
-##
-
-echo "Create uglified library..."
-node ./node_modules/uglify-js/bin/uglifyjs ./src/bitcoinjs.modified.js > ./dist/bitcoinjs.min.js
-echo "Library uglified into to ./dist/bitcoinjs.min.js"
